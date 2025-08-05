6dfa512530f5dafef1c14d71ca9c882bb16b8559
Geovanni Gonzalez <geovanniga32@gmail.com>
2025-08-04 20:08:03 -0600

    Subida de archivos

---
+++ src/main.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+
+const HTML_FILE = 'Calculadora.html';
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: "Calculadora Multibase",
+        width: 600,
+        height: 850,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+        }
+    });
+    mainWindow.loadFile(path.join(__dirname, HTML_FILE));
+}
+
+app.whenReady().then(createMainWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+});
